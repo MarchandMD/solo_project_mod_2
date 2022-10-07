9f9ef180430f13ae10a8a4e665ab30943856aecf
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-10-07 09:59:40 -0600

    Add routes

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/mountains', to: 'mountains#index'
+  get '/mountains/:id', to: 'mountains#show'
+  get '/trails', to: 'trails#index'
+  get '/trails/:id', to: 'trails#show'
+  get '/mountains/:mountain_id/trails', to: 'mountains_trails#index'
+end
